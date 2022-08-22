d68ea34c223eaf2f6af344a4132b4aee26ce47b9
clwmsms12 <clwmsms12@naver.com>
2022-08-22 15:17:55 +0900

    2022theme

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+if (!defined('_INDEX_')) define('_INDEX_', true);
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+if (G5_IS_MOBILE) {
+    include_once(G5_THEME_MOBILE_PATH.'/index.php');
+    return;
+}
+
+if(G5_COMMUNITY_USE === false) {
+    include_once(G5_THEME_SHOP_PATH.'/index.php');
+    return;
+}
+
+include_once(G5_THEME_PATH.'/head.php');
+?>
+<h2>test</h2>
+
+<?php
+echo G5_THEME_PATH;
+echo "<br>";
+echo G5_THEME_IMG_URL;
+echo "<br>";
+echo G5_THEME_CSS_URL;
+echo "<br>";
+echo G5_THEME_JS_URL;
+echo "<br>";
+?>
+<div class="wrap slideWrap">
+	<ul	class="slide">
+		<li>
+			<img src="<? echo G5_THEME_IMG_URL ?>/img1.jpg" alt="">
+			<img src="<? echo G5_THEME_IMG_URL ?>/img2.jpg" alt="">
+			<img src="<? echo G5_THEME_IMG_URL ?>/img3.jpg" alt="">
+			<img src="<? echo G5_THEME_IMG_URL ?>/img4.jpg" alt="">
+		</li>
+	</ul>
+</div>
+
+<script>
+	$(".slide").bxSlider()
+</script>
+
+
+<?php
+include_once(G5_THEME_PATH.'/tail.php');
